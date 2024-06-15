69382eec19303702f60118c687ebf5674c9206a2
vanek021 <vanekmenyakin@gmail.com>
2024-06-15 07:45:52 +0500

    + Новые нужные запросы

---
+++ GigaComic.Shared/Responses/Comic/ComicResultResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaComic.Shared.Responses.Comic
+{
+    /// <summary>
+    /// Расширение базовой модели, здесь будет финальный результат комикста с картинками
+    /// </summary>
+    public class ComicResultResponse : IResponse
+    {
+    }
+}
--- GigaComic.Shared/Routes/ComicEndpoints.cs
+++ GigaComic.Shared/Routes/ComicEndpoints.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GigaComic.Shared.Constants;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,14 @@ namespace GigaComic.Shared.Routes
         public const string CompleteAbstractCreationStage = "api/Comic/CompleteAbstractCreationStage";
         public const string CompleteStoriesCreationStage = "api/Comic/CompleteStoriesCreationStage";
         public const string CompleteSetupStage = "api/Comic/CompleteSetupStage";
+        public const string RegenerateAbstractStory = "api/Comic/RegenerateAbstractStory";
+        public const string ComicResult = "api/Comic/ComicResult";
+        public const string ComicResults = "api/Comic/ComicResult";
+
+        public static string GetComicResult(Guid id)
+            => $"{ComicResult}?id={id}";
+
+        public static string GetComicResults(Guid id, int page = 1, int pageSize = PageConstants.DefaultPageSize)
+            => $"{ComicResults}?id={id}&page={page}&pageSize={pageSize}";
     }
 }
