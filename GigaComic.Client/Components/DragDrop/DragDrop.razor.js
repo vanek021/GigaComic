eafa68dff7b473412811a8e6132b438d1bae6bf5
vanek021 <vanekmenyakin@gmail.com>
2024-06-16 12:57:02 +0500

    * Фикс назначения драг-ивентов

--- GigaComic.Client/Components/DragDrop/DragDrop.razor.js
+++ GigaComic.Client/Components/DragDrop/DragDrop.razor.js
@@ -1,67 +1,64 @@
-function initDragEvents() {
+function initDragEvent(drag) {
     var app = document.getElementById('app');
+    console.log(drag);
 
-    $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("dragstart", function (e) {
-            var hideDragImage = this.cloneNode(true);
-            hideDragImage.id = "hideDragImage-hide"
+    drag.addEventListener("dragstart", function (e) {
+        var hideDragImage = this.cloneNode(true);
+        hideDragImage.id = "hideDragImage-hide"
 
-            var dragImage = this.cloneNode(true);
-            dragImage.id = "draggeimage";
-            dragImage.style.position = "absolute";
+        var dragImage = this.cloneNode(true);
+        dragImage.id = "draggeimage";
+        dragImage.style.position = "absolute";
 
-            hideDragImage.style.opacity = 0;
-            hideDragImage.style.display = "none";
+        hideDragImage.style.opacity = 0;
+        hideDragImage.style.display = "none";
 
-            dragImage.style.left = "-999px";
-            dragImage.style.top = "-999px";
+        dragImage.style.left = "-999px";
+        dragImage.style.top = "-999px";
 
-            app.appendChild(hideDragImage);
-            app.appendChild(dragImage);
-            e.dataTransfer.setDragImage(hideDragImage, 0, 0);
-        }, false);
+        app.appendChild(hideDragImage);
+        app.appendChild(dragImage);
+        e.dataTransfer.setDragImage(hideDragImage, 0, 0);
+    }, false);
+
+    drag.addEventListener("dragend", function (e) {
+        var hideDragImage = document.getElementById('hideDragImage-hide');
+        var dragImage = document.getElementById('draggeimage');
+        hideDragImage.remove()
+        dragImage.remove()
+    }, false);
+
+    drag.addEventListener("drag", function (e) {
+        dragImage = document.getElementById('draggeimage');
+        if (dragImage) {
+            dragImage.style.left = e.pageX + 5 + "px";
+            dragImage.style.top = e.pageY + 5 + "px";
+        }
     });
 
-    $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("dragend", function (e) {
-            var hideDragImage = document.getElementById('hideDragImage-hide');
-            var dragImage = document.getElementById('draggeimage');
-            hideDragImage.remove()
-            dragImage.remove()
-        }, false);
+    drag.addEventListener("dragenter", function (e) {
+        e.target.style.opacity = "0.3";
     });
 
-    $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("drag", function (e) {
-            dragImage = document.getElementById('draggeimage');
-            if (dragImage) {
-                dragImage.style.left = e.pageX + 5 + "px";
-                dragImage.style.top = e.pageY + 5 + "px";
-            }
-        });
+    drag.addEventListener("dragleave", function (e) {
+        e.target.style.opacity = "1";
     });
 
-    $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("dragenter", function (e) {
-            e.target.style.opacity = "0.3";
-        });
+    drag.addEventListener("drop", function (e) {
+        e.target.style.opacity = "1";
     });
+}
 
+function initDragEvents() {
     $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("dragleave", function (e) {
-            e.target.style.opacity = "1";
-        });
+        initDragEvents(this);
     });
+}
 
-    $(".dragdrop__item").each(function () {
-        var drag = this;
-        drag.addEventListener("drop", function (e) {
-            e.target.style.opacity = "1";
-        });
-    });
+function initDragEventById(id) {
+    console.log(id);
+    var el = $(`#${id}`);
+    console.log('el');
+    console.log(el[0]);
+    initDragEvent($(`#${id}`)[0]);
 }
