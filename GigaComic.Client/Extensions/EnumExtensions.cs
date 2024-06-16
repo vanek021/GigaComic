ad24131698065fb8e9e5bcf3c178918954108c88
vanek021 <vanekmenyakin@gmail.com>
2024-06-16 21:29:27 +0500

    + Вывод статуса комикса

---
+++ GigaComic.Client/Extensions/EnumExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace GigaComic.Client.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+              .GetMember(enumValue.ToString())
+              .First()
+              .GetCustomAttribute<DisplayAttribute>()
+              ?.GetName()!;
+        }
+
+        public static List<T> GetOrderedValues<T>()
+            where T : Enum
+        {
+            return GetValues<T>().OrderBy(e => ((DisplayAttribute)typeof(T)
+                .GetMember(e.ToString())[0]
+                .GetCustomAttributes(typeof(DisplayAttribute), false)[0]).Order)
+                .ToList();
+        }
+
+        public static List<T> GetValues<T>()
+            where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToList();
+        }
+    }
+}
